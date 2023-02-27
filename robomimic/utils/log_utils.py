87494c2644b904dfdecf3e36662e5116fc7806c9
Vaibhav Saxena <saxena.vaibhav96@gmail.com>
2023-02-27 10:02:23 -0500

    added code for affordance map computation and logging

--- robomimic/utils/log_utils.py
+++ robomimic/utils/log_utils.py
@@ -8,6 +8,7 @@ import numpy as np
 from datetime import datetime
 from contextlib import contextmanager
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 
 class PrintLogger(object):
@@ -43,6 +44,7 @@ class DataLogger(object):
         """
         self._tb_logger = None
         self._data = dict() # store all the scalar data logged so far
+        self.log_dir = log_dir
 
         if log_tb:
             from tensorboardX import SummaryWriter
@@ -80,6 +82,61 @@ class DataLogger(object):
                         self._tb_logger.add_scalar(stat_k_name, stat_v, epoch)
             elif data_type == 'image':
                 self._tb_logger.add_images(k, img_tensor=v, global_step=epoch, dataformats="NHWC")
+
+    def save_3d_affordance_grid(self, affordances, name_prefix, name_postfix):
+        # TODO(VS) plot all affordances
+        aff = affordances[-3].detach().cpu().numpy() # plotting one obs only
+
+        import pylab
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        colmap = pylab.cm.ScalarMappable(cmap=pylab.cm.viridis)
+        colmap.set_array(aff[:,3])
+        ax.scatter(aff[:,0], aff[:,1], aff[:,2], c=pylab.cm.viridis((aff[:,3]-min(aff[:,3]))/(max(aff[:,3])-min(aff[:,3]))), marker='o')
+        fig.colorbar(colmap)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        os.makedirs(f'{self.log_dir}/affordances', exist_ok=True)
+        plt.savefig(f'{self.log_dir}/affordances/{name_prefix}_affordances_{name_postfix}.png')
+        plt.close()
+
+    def save_2d_affordances_and_images(self, affordances, input_images, image_recons, name_prefix, name_postfix):
+        for k in input_images: # all image keys in the input
+            imgs = input_images[k]
+            num_rows = 2
+
+            if image_recons is not None:
+                recons = image_recons[k]
+                num_rows = 3
+
+            num_imgs = len(imgs)
+            fig, ax = plt.subplots(num_rows, num_imgs)
+            if num_imgs == 1:
+                ax = np.array([ax]).transpose()
+
+            for col in range(num_imgs):
+                aff = affordances[col, ..., -1]
+                img = imgs[col].transpose([1,2,0])
+                # aff = aff[..., -1] # others are just coordinates of the corresponding action
+                if image_recons is not None:
+                    recon = recons[col].transpose([1,2,0])
+                    ax[2,col].imshow(recon)
+                    ax[2,col].set_xlabel('X')
+                    ax[2,col].set_ylabel('Y')
+                    ax[2,col].set_title('recon')
+
+                ax[0,col].imshow(img)
+                plot = ax[1,col].imshow(aff, cmap='viridis'); plt.colorbar(plot, ax=ax[1,col])
+
+                ax[0,col].set_xlabel('X'); ax[1,col].set_xlabel('X')
+                ax[0,col].set_ylabel('Y'); ax[1,col].set_ylabel('Y')
+                ax[0,col].set_title('input'); ax[1,col].set_title('afford')
+
+            os.makedirs(f'{self.log_dir}/affordances', exist_ok=True)
+            plt.savefig(f'{self.log_dir}/affordances/{name_prefix}_affordances_{k}_{name_postfix}.png')
+            plt.close()
 
     def get_stats(self, k):
         """
